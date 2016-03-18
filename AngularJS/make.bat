d1a32fe72db5140ce5cf89a92bc41baf50c5d05a
Mikalai Karabeika <hexed2@gmail.com>
2016-03-18 10:11:09 +0300

    Delete make.bat

--- AngularJS/make.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-@echo|set /p=Make AngularJS
-copy /b "..\..\..\Stimulsoft.Reports.JS\Builder\complete\css\min\stimulsoft.viewer.office2013.css" "reports\stimulsoft.viewer.office2013.css" >nul
-
-copy /b "..\..\..\Stimulsoft.Reports.JS\Builder\complete\scripts\min\stimulsoft.reports.js" "reports\stimulsoft.reports.js" >nul
-copy /b "..\..\..\Stimulsoft.Reports.JS\Builder\complete\scripts\min\stimulsoft.viewer.js" "reports\stimulsoft.viewer.js" >nul
-@echo  [OK]
