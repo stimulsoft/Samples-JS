09301c2e5ddfe5baa1805ed894d91d75e97d011d
Mikalai Karabeika <hexed2@gmail.com>
2016-03-18 10:12:18 +0300

    Delete make.bat

--- Demo/make.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@echo|set /p=Make Demo
-copy /b "..\..\Stimulsoft.Reports.JS\Builder\complete\css\min\stimulsoft.designer.office2013.white.blue.css" "css\stimulsoft.designer.office2013.white.blue.css" >nul
-copy /b "..\..\Stimulsoft.Reports.JS\Builder\complete\css\min\stimulsoft.viewer.office2013.css" "css\stimulsoft.viewer.office2013.css" >nul
-
-copy /b "..\..\Stimulsoft.Reports.JS\Builder\complete\scripts\min\stimulsoft.designer.js" "scripts\stimulsoft.designer.js" >nul
-copy /b "..\..\Stimulsoft.Reports.JS\Builder\complete\scripts\min\stimulsoft.reports.js" "scripts\stimulsoft.reports.js" >nul
-copy /b "..\..\Stimulsoft.Reports.JS\Builder\complete\scripts\min\stimulsoft.viewer.js" "scripts\stimulsoft.viewer.js" >nul
-@echo  [OK]
