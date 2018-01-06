adc3274a92050de105c1e5a8b3cacc12d0491f80
kubekin2 <vkubekin@gmail.com>
2018-01-06 17:47:43 +0300

    Update Samples to 2018.1.4

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,35 +1,40 @@
 var http = require('http'),
-url = require('url'),
-path = require('path'),
-fs = require('fs');
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
 
 var mimeTypes = {
-  "html": "text/html",
-  "js": "text/javascript",
-  "css": "text/css"};
+    "html": "text/html",
+    "js": "text/javascript",
+    "css": "text/css"
+};
 
-http.createServer(function(req, res) {
+http.createServer(function (req, res) {
     var uri = decodeURI(url.parse(req.url).pathname);
     var filename = path.join(process.cwd(), uri);
-	
-	if (fs.statSync(filename).isDirectory()) filename += '/index.html';
-	
-    fs.exists(filename, function(exists) {
-        if(!exists) {
-            console.log("not exists: " + filename);
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.write('404 Not Found\n');
-            res.end();
-            return;
-        }
-		
-        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
-        res.writeHead(200, {'Content-Type':mimeType});
+    try {
+        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
 
-        var fileStream = fs.createReadStream(filename);
-        fileStream.pipe(res);
+        fs.exists(filename, function (exists) {
+            if (!exists) {
+                console.log("not exists: " + filename);
+                res.writeHead(200, { 'Content-Type': 'text/plain' });
+                res.write('404 Not Found\n');
+                res.end();
+                return;
+            }
 
-    });
+            var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+            if (mimeType) res.writeHead(200, { 'Content-Type': mimeType });
+
+            var fileStream = fs.createReadStream(filename);
+            fileStream.pipe(res);
+
+        });
+    }
+    catch (e) {
+        return;
+    }
 }).listen(8888);
 
 
