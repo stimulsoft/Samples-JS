8619900ed1c21f43786f656cf0c7ccf7c4c673ea
Yuriy Kubekin <vkubekin@gmail.com>
2020-11-07 12:04:08 +0300

    Update Sample

--- Node.js/06. Load Report from Server Side/index.js
+++ Node.js/06. Load Report from Server Side/index.js
@@ -10,29 +10,29 @@ function accept(req, res) {
     //Send index.html
     if (req.url == "/") {
         res.writeHeader(200, { "Content-Type": "text/html" });
-        res.end(fs.readFileSync("web/index.html"));
+        res.end(fs.readFileSync("index.html"));
     }
     //Send style.css
     else if (req.url == "/stimulsoft.viewer.office2013.whiteblue.css") {
         res.writeHeader(200, { "Content-Type": "text/css" });
-        res.end(fs.readFileSync("web/stimulsoft.viewer.office2013.whiteblue.css"));
+        res.end(fs.readFileSync("node_modules/stimulsoft-reports-js/Css/stimulsoft.viewer.office2013.whiteblue.css"));
     }
     //Send reports.js
     else if (req.url == "/stimulsoft.reports.js") {
         res.writeHeader(200, { "Content-Type": "text/javascript" });
-        res.end(fs.readFileSync("web/stimulsoft.reports.js"));
+        res.end(fs.readFileSync("node_modules/stimulsoft-reports-js/Scripts/stimulsoft.reports.js"));
     }
     //Send viewer.js
     else if (req.url == "/stimulsoft.viewer.js") {
         res.writeHeader(200, { "Content-Type": "text/javascript" });
-        res.end(fs.readFileSync("web/stimulsoft.viewer.js"));
+        res.end(fs.readFileSync("node_modules/stimulsoft-reports-js/Scripts/stimulsoft.viewer.js"));
     }
     //Send report
     else if (req.url == "/getReport") {
         // Creating new report
         var report = new Stimulsoft.Report.StiReport();
         // Loading report template
-        report.loadFile("report/SimpleList.mrt");
+        report.loadFile("SimpleList.mrt");
         // Renreding report
         report.renderAsync(function () {
             // Saving rendered report to JSON string
