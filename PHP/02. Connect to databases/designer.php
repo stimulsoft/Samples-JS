b4c45b7f67d1834fbb620f8fb9c9e1c15ef83160
Yuriy Kubekin <vkubekin@gmail.com>
2020-11-11 11:11:31 +0300

    Update samples

--- PHP/02. Connect to databases/designer.php
+++ PHP/02. Connect to databases/designer.php
@@ -1,12 +1,8 @@
-<?php
-require_once 'stimulsoft/helper.php';
-?>
-
 <!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Stimulsoft Reports.PHP - JS Designer</title>
+	<title>Stimulsoft Reports.JS Designer</title>
 
 	<!-- Office2013 style -->
 	<link href="css/stimulsoft.viewer.office2013.whiteblue.css" rel="stylesheet">
@@ -18,37 +14,21 @@ require_once 'stimulsoft/helper.php';
 	<!-- Stimulsoft JS Viewer (for preview tab) and Stimulsoft JS Designer-->
 	<script src="scripts/stimulsoft.viewer.js" type="text/javascript"></script>
 	<script src="scripts/stimulsoft.designer.js" type="text/javascript"></script>
-	
-	<?php 
-		$options = StiHelper::createOptions();
-		$options->handler = "handler.php";
-		$options->timeout = 30;
-		StiHelper::initialize($options);
-	?>
+
 	<script type="text/javascript">
-		Stimulsoft.Base.StiLicense.loadFromFile("stimulsoft/license.php");
+		Stimulsoft.StiOptions.WebServer.url = "stimulsoft/handler.php";
 		
 		var options = new Stimulsoft.Designer.StiDesignerOptions();
 		options.appearance.fullScreenMode = true;
 		options.toolbar.showSendEmailButton = true;
-		
+
 		var designer = new Stimulsoft.Designer.StiDesigner(options, "StiDesigner", false);
-		
-		// Process SQL data source
-		designer.onBeginProcessData = function (event, callback) {
-			<?php StiHelper::createHandler(); ?>
-		}
-		
-		// Save report template on the server side
-		designer.onSaveReport = function (event) {
-			<?php StiHelper::createHandler(); ?>
-		}
-		
+
 		// Load and design report
 		var report = new Stimulsoft.Report.StiReport();
 		report.loadFile("reports/SimpleList.mrt");
 		designer.report = report;
-		
+
 		function onLoad() {
 			designer.renderHtml("designerContent");
 		}
--- PHP/02. Connect to databases/viewer.php
+++ PHP/02. Connect to databases/viewer.php
@@ -1,12 +1,9 @@
-<?php
-require_once 'stimulsoft/helper.php';
-?>
 
 <!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title>Stimulsoft Reports.PHP - JS Viewer</title>
+	<title>Stimulsoft Reports.JS Viewer</title>
 
 	<!-- Office2013 style -->
 	<link href="css/stimulsoft.viewer.office2013.whiteblue.css" rel="stylesheet">
@@ -16,49 +13,21 @@ require_once 'stimulsoft/helper.php';
 
 	<!-- Stimulsoft JS Viewer -->
 	<script src="scripts/stimulsoft.viewer.js" type="text/javascript"></script>
-	
-	<?php 
-		$options = StiHelper::createOptions();
-		$options->handler = "handler.php";
-		$options->timeout = 30;
-		StiHelper::initialize($options);
-	?>
+
 	<script type="text/javascript">
-		Stimulsoft.Base.StiLicense.loadFromFile("stimulsoft/license.php");
-		
+		Stimulsoft.StiOptions.WebServer.url = "stimulsoft/handler.php";
+
 		var options = new Stimulsoft.Viewer.StiViewerOptions();
 		options.appearance.fullScreenMode = true;
 		options.toolbar.showSendEmailButton = true;
-		
+
 		var viewer = new Stimulsoft.Viewer.StiViewer(options, "StiViewer", false);
-		
-		// Process SQL data source
-		viewer.onBeginProcessData = function (event, callback) {
-			<?php StiHelper::createHandler(); ?>
-		}
-		
-		// Manage export settings on the server side
-		viewer.onBeginExportReport = function (args) {
-			<?php //StiHelper::createHandler(); ?>
-			//args.fileName = "MyReportName";
-		}
-		
-		// Process exported report file on the server side
-		/*viewer.onEndExportReport = function (event) {
-			event.preventDefault = true; // Prevent client default event handler (save the exported report as a file)
-			<?php StiHelper::createHandler(); ?>
-		}*/
-		
-		// Send exported report to Email
-		viewer.onEmailReport = function (event) {
-			<?php StiHelper::createHandler(); ?>
-		}
-		
+
 		// Load and show report
 		var report = new Stimulsoft.Report.StiReport();
 		report.loadFile("reports/SimpleList.mrt");
 		viewer.report = report;
-		
+
 		function onLoad() {
 			viewer.renderHtml("viewerContent");
 		}
