0fb6c02bc34c9bc42c29280eab2cc496478e66c6
Ivan Trusov <i.l.trusov@gmail.com>
2022-03-09 09:51:38 +0300

    Vue 3 samples
    https://github.com/stimulsoft/Stimulsoft.Reports/issues/6379

---
+++ Vue.js 3/Integrating the Report Designer into an Application/src/App.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { Stimulsoft } from "stimulsoft-reports-js/Scripts/stimulsoft.blockly.editor";
+import 'stimulsoft-reports-js/Css/stimulsoft.designer.office2013.whiteblue.css';
+import 'stimulsoft-reports-js/Css/stimulsoft.viewer.office2013.whiteblue.css';
+
+onMounted(() => {
+  console.log('Loading Designer view');
+
+    console.log('Set full screen mode for the designer');
+    var options = new Stimulsoft.Designer.StiDesignerOptions();
+    options.appearance.fullScreenMode = false;
+
+    console.log('Create the report designer with specified options');
+    var designer = new Stimulsoft.Designer.StiDesigner(options, 'StiDesigner', false);
+
+    console.log('Create a new report instance');
+    var report = new Stimulsoft.Report.StiReport();
+
+    console.log('Load report from url');
+    report.loadFile('reports/SimpleList.mrt');
+
+    console.log('Edit report template in the designer');
+    designer.report = report;
+
+    console.log('Rendering the viewer to selected element');
+    designer.renderHtml('designer');
+
+    console.log('Loading completed successfully!');
+});</script>
+
+<template>
+  <div id="app">
+    <div>
+      <h2>Stimulsoft Reports.JS Designer</h2>
+      <div id="designer"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Vue.js 3/Integrating the Report Viewer into an Application/src/App.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { Stimulsoft } from "stimulsoft-reports-js/Scripts/stimulsoft.blockly.editor";
+import 'stimulsoft-reports-js/Css/stimulsoft.viewer.office2013.whiteblue.css';
+
+onMounted(() => {
+  console.log('Loading Viewer view');
+
+  console.log('Creating the report viewer with default options');
+  const viewer = new Stimulsoft.Viewer.StiViewer(undefined, 'StiViewer', false);
+
+  console.log('Creating a new report instance');
+
+  const report = new Stimulsoft.Report.StiReport();
+
+  console.log('Load report from url');
+  report.loadFile('/reports/SimpleList.mrt');
+
+  console.log('Assigning report to the viewer, the report will be built automatically after rendering the viewer');
+  viewer.report = report;
+
+  console.log('Rendering the viewer to selected element');
+  viewer.renderHtml('viewer');
+
+  console.log('Loading completed successfully!');
+});</script>
+
+<template>
+  <div id="app">
+    <div>
+      <h2>Stimulsoft Reports.JS Viewer</h2>
+      <div id="viewer"></div>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
