0ba9a47ec072c2115f1a13f1bb7ff6e1e9de0c68
Tim Kubekin <tima.kubekin156@gmail.com>
2022-03-22 11:24:46 +0300

    Some fixes in samples

--- Vue.js 3/Integrating the Report Designer into an Application/src/App.vue
+++ Vue.js 3/Integrating the Report Designer into an Application/src/App.vue
@@ -1,32 +1,32 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { Stimulsoft } from "stimulsoft-reports-js/Scripts/stimulsoft.blockly.editor";
-import 'stimulsoft-reports-js/Css/stimulsoft.designer.office2013.whiteblue.css';
-import 'stimulsoft-reports-js/Css/stimulsoft.viewer.office2013.whiteblue.css';
+import "stimulsoft-reports-js/Css/stimulsoft.designer.office2013.whiteblue.css";
+import "stimulsoft-reports-js/Css/stimulsoft.viewer.office2013.whiteblue.css";
 
 onMounted(() => {
-  console.log('Loading Designer view');
+  console.log("Loading Designer view");
 
-  console.log('Set full screen mode for the designer');
-  var options = new Stimulsoft.Designer.StiDesignerOptions();
+  console.log("Set full screen mode for the designer");
+  let options = new Stimulsoft.Designer.StiDesignerOptions();
   options.appearance.fullScreenMode = false;
 
-  console.log('Create the report designer with specified options');
-  var designer = new Stimulsoft.Designer.StiDesigner(options, 'StiDesigner', false);
+  console.log("Create the report designer with specified options");
+  let designer = new Stimulsoft.Designer.StiDesigner(options, "StiDesigner", false);
 
-  console.log('Create a new report instance');
-  var report = new Stimulsoft.Report.StiReport();
+  console.log("Create a new report instance");
+  let report = new Stimulsoft.Report.StiReport();
 
-  console.log('Load report from url');
-  report.loadFile('reports/SimpleList.mrt');
+  console.log("Load report from url");
+  report.loadFile("reports/SimpleList.mrt");
 
-  console.log('Edit report template in the designer');
+  console.log("Edit report template in the designer");
   designer.report = report;
 
-  console.log('Rendering the designer to selected element');
-  designer.renderHtml('designer');
+  console.log("Rendering the designer to selected element");
+  designer.renderHtml("designer");
 
-  console.log('Loading completed successfully!');
+  console.log("Loading completed successfully!");
 });
 </script>
 
--- Vue.js 3/Integrating the Report Viewer into an Application/src/App.vue
+++ Vue.js 3/Integrating the Report Viewer into an Application/src/App.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
 import { Stimulsoft } from "stimulsoft-reports-js/Scripts/stimulsoft.blockly.editor";
-import 'stimulsoft-reports-js/Css/stimulsoft.viewer.office2013.whiteblue.css';
+import "stimulsoft-reports-js/Css/stimulsoft.viewer.office2013.whiteblue.css";
 
 onMounted(() => {
-  console.log('Loading Viewer view');
+  console.log("Loading Viewer view");
 
-  console.log('Creating the report viewer with default options');
-  const viewer = new Stimulsoft.Viewer.StiViewer(undefined, 'StiViewer', false);
+  console.log("Creating the report viewer with default options");
+  let viewer = new Stimulsoft.Viewer.StiViewer(undefined, "StiViewer", false);
 
-  console.log('Creating a new report instance');
+  console.log("Creating a new report instance");
 
-  const report = new Stimulsoft.Report.StiReport();
+  let report = new Stimulsoft.Report.StiReport();
 
-  console.log('Load report from url');
-  report.loadFile('/reports/SimpleList.mrt');
+  console.log("Load report from url");
+  report.loadFile("/reports/SimpleList.mrt");
 
-  console.log('Assigning report to the viewer, the report will be built automatically after rendering the viewer');
+  console.log("Assigning report to the viewer, the report will be built automatically after rendering the viewer");
   viewer.report = report;
 
-  console.log('Rendering the viewer to selected element');
-  viewer.renderHtml('viewer');
+  console.log("Rendering the viewer to selected element");
+  viewer.renderHtml("viewer");
 
-  console.log('Loading completed successfully!');
+  console.log("Loading completed successfully!");
 });
 </script>
 
