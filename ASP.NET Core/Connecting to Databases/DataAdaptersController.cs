6fcb223afbcf2cc147b0fd512311b33dddbd1da1
Serge Puhov <musevitebsk@gmail.com>
2022-08-04 13:54:55 +0300

    new release v2022.3.4

--- ASP.NET Core/Connecting to Databases/DataAdaptersController.cs
+++ ASP.NET Core/Connecting to Databases/DataAdaptersController.cs
@@ -1,7 +1,7 @@
 /*
 Stimulsoft.Reports.JS
-Version: 2022.3.2
-Build date: 2022.06.23
+Version: 2022.3.4
+Build date: 2022.08.02
 License: https://www.stimulsoft.com/en/licensing/reports
 */
 using FirebirdSql.Data.FirebirdClient;
@@ -137,7 +137,7 @@ namespace NetCoreDataAdapters
                 result.Notice = e.Message;
             }
 
-            result.HandlerVersion = "2022.3.2";
+            result.HandlerVersion = "2022.3.4";
             result.CheckVersion = true;
 
             var contentType = "application/json";
