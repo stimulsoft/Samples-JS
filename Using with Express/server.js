25061812f717bf308fd4fdf8504f299c5cd3cada
Serge Puhov <musevitebsk@gmail.com>
2023-10-17 17:51:33 +0200

    Some Improvements
    https://github.com/stimulsoft/Stimulsoft.Reports/issues/12788

--- Using with Express/server.js
+++ Using with Express/server.js
@@ -26,6 +26,10 @@ app.get('/stimulsoft.designer.js', (req, res) => {
     res.sendFile(__dirname + "/node_modules/stimulsoft-reports-js/Scripts/stimulsoft.designer.js");
   });
 
+app.get('/stimulsoft.blockly.editor.js', (req, res) => {
+    res.sendFile(__dirname + "/node_modules/stimulsoft-reports-js/Scripts/stimulsoft.blockly.editor.js");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
   console.log(`Navigate to http://localhost:${port}/viewer show Viewer`);
